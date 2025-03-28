2823acb9e7e6130a09ac34967b1c4a5d338e5445
lloljkbdf1 <104715375+lloljkbdf1@users.noreply.github.com>
2025-03-28 20:33:46 +1100

    Add files via upload

---
+++ wayaya.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+root=tk.Tk()
+root.geometry("100x100+25+25")
+root.minsize(width=500,height=500)
+tk.Button(root,activebackground="white",default="active",text="Boo! did i scare you?",bg="black",width=20,height=10,font="Arial,50").pack()
+tk.Entry(root,bd=100,bg="white",width=10000,state="normal",font="Arial,100",).pack()
+tk.Label(root,text="hi leo",relief="sunken",width=20,height=11,bg="gray",font="Arial,100").pack()
+root.title("Hi Leo")
+root.maxsize(width=1000,height=1000)
+tk.mainloop()
